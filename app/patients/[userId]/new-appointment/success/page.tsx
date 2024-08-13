3f63ec6a511cce1706b5ffe434d78e8706984f93
Jakub Satora <kubasatora@wp.pl>
2024-08-13 15:32:53 +0200

    appointment

--- app/patients/[userId]/new-appointment/success/page.tsx
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -1,8 +1,18 @@
+import { Button } from '@/components/ui/button'
+import { Doctors } from '@/constants'
+import { getAppointment } from '@/lib/actions/appointment.actions'
+import { formatDateTime } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { formatDate } from 'react-datepicker/dist/date_utils'
 
-const Success = () => {
+const Success = async ({ params: { userId }, searchParams }: SearchParamProps) => {
+    const appontmentId = (searchParams?.appointmentId as string) || ""
+    const appointment = await getAppointment(appontmentId)
+
+    const doctor = Doctors.find((doc) => doc.name === appointment.primaryPhysician)
+
     return (
         <div className='flex h-screen max-h-screen px-[5%]'>
             <div className='success-img'>
@@ -34,10 +44,39 @@ const Success = () => {
                     <p>
                         Requested appointment details:
                     </p>
-<div className='flex items-center gap-3'>
-{/* <Image/> */}
-</div>
+                    <div className='flex items-center gap-3'>
+                        <Image
+                            src={doctor?.image!}
+                            alt="doctor"
+                            width={100}
+                            height={100}
+                            className='size-6'
+                        />
+                        <p className='whitespace-nowrap'>
+                            Dr. {doctor?.name}
+                        </p>
+
+                    </div>
+                    <div className="flex gap-2">
+                        <Image
+                            src="/assets/icons/calendar.svg"
+                            height={24}
+                            width={25}
+                            alt='calendar'
+                        />
+                        <p>
+                            {formatDateTime(appointment.schedule).dateTime}
+                        </p>
+                    </div>
                 </section>
+                <Button variant="outline" className='shad-primary-btn' asChild>
+                    <Link href={`/patients/${userId}/new-appointment`}>
+                        New Appointment
+                    </Link>
+                </Button>
+                <p className='copyright'>
+                    © 2024 CarePuse
+                </p>
             </div>
         </div>
     )
--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -3,15 +3,28 @@ import { ID } from "node-appwrite"
 import { APPOINTMENT_COLLECTION_ID, DATABASE_ID, databases, ENDPOINT } from "../appwrite.config"
 import { parseStringify } from "../utils"
 
-export const CreateAppointment=async (appointment:CreateAppointmentParams)=>{
+export const CreateAppointment = async (appointment: CreateAppointmentParams) => {
     try {
         const newAppointment = await databases.createDocument(
             DATABASE_ID!,
             APPOINTMENT_COLLECTION_ID!,
             ID.unique(),
             appointment
-          )
-          return parseStringify(newAppointment)
+        )
+        return parseStringify(newAppointment)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const getAppointment = async (appointmentId: string) => {
+    try {
+        const appointment = await databases.getDocument(
+            DATABASE_ID!,
+            APPOINTMENT_COLLECTION_ID!,
+            appointmentId
+        )
+        return parseStringify(appointment)
     } catch (error) {
         console.log(error)
     }
